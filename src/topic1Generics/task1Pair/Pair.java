7707229744ea7c793df775d2cbabe4810dd4de52
Capmashine <xxxcap13@gmail.com>
2025-05-12 21:23:37 +0300

    qwe

--- src/topic1Generics/task1Pair/Pair.java
+++ src/topic1Generics/task1Pair/Pair.java
@@ -1,2 +1,32 @@
-package topic1Generics.task1Pair;public class Pair {
+package topic1Generics.task1Pair;
+
+public class Pair <T,U>{
+
+    private T first;
+    private U second;
+
+    public Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public U getSecond() {
+        return second;
+    }
+
+    public void setSecond(U second) {
+        this.second = second;
+    }
+
+    public String toString(){
+        return "Pair: " + first + " " + second;
+    }
 }
--- src/topic1Generics/task1Pair/PairDemo.java
+++ src/topic1Generics/task1Pair/PairDemo.java
@@ -1,2 +1,14 @@
-package topic1Generics.task1Pair;public class PairDemo {
+package topic1Generics.task1Pair;
+
+public class PairDemo {
+    public static void main(String[] args) {
+
+        Pair<String,Integer> pair = new Pair<>("Ca",32);
+        System.out.println(pair);
+
+
+        pair.setFirst("Max");
+        pair.setSecond(32);
+        System.out.println(pair);
+    }
 }
