c6f54c53300e68987329ea8ee22e54fd7d393047
Capmashine <xxxcap13@gmail.com>
2025-05-12 23:50:15 +0300

    topic 1 task4 is done

--- src/topic1Generics/task4GenericUtils/GenericUtilsDemo.java
+++ src/topic1Generics/task4GenericUtils/GenericUtilsDemo.java
@@ -1,2 +1,14 @@
-package topic1Generics.task4GenericUtils;public class GenericUtilsDemo {
+package topic1Generics.task4GenericUtils;
+
+public class GenericUtilsDemo {
+    public static void main(String[] args) {
+
+
+        Integer max = GenericUtils.max(13, 23);
+        System.out.println(max);
+
+        String max1 = GenericUtils.max("hi", "Hello");
+        System.out.println(max1);
+
+    }
 }
