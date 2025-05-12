b3a977b8bab0631da75e6ac4c7456bd75c31d92b
Capmashine <xxxcap13@gmail.com>
2025-05-12 21:50:59 +0300

    task2 is done

--- src/topic1Generics/task2ArrayUtils/ArrayUtilsDemo.java
+++ src/topic1Generics/task2ArrayUtils/ArrayUtilsDemo.java
@@ -1,2 +1,18 @@
-package topic1Generics.task2ArrayUtils;public class ArrayUtilsDemo {
+package topic1Generics.task2ArrayUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayUtilsDemo {
+    public static void main(String[] args) {
+
+        Integer[] array = {1, 2, 3, 4, 5};
+
+        System.out.println(Arrays.toString(array));
+
+        ArrayUtils.swap(array, 1, 3);
+
+        System.out.println(Arrays.toString(array));
+
+    }
 }
