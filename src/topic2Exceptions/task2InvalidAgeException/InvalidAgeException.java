9c52acbd72babc45c26188ddcbecd1bf2abef073
Capmashine <xxxcap13@gmail.com>
2025-05-14 17:36:34 +0300

    topic2 tsk2 is done

---
+++ src/topic2Exceptions/task2InvalidAgeException/InvalidAgeException.java
@@ -0,0 +1,9 @@
+package topic2Exceptions.task2InvalidAgeException;
+
+public class InvalidAgeException extends Exception {
+
+    public InvalidAgeException(String msg) {
+        super(msg);
+    }
+}
+
