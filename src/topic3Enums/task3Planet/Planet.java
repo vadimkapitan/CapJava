da327a206c4bfd4872efcb76089bf60d554d5273
Capmashine <xxxcap13@gmail.com>
2025-05-16 16:11:59 +0300

    topic3 task3 is done

--- src/topic3Enums/task3Planet/Planet.java
+++ src/topic3Enums/task3Planet/Planet.java
@@ -1,2 +1,32 @@
-package topic3Enums.task3Planet;public enum Planet {
+package topic3Enums.task3Planet;
+
+/**
+ * Создайте перечисление Planet с элементами (например, EARTH, MARS, JUPITER) и следующими параметрами:
+ * mass (масса в кг)
+ * radius (радиус в метрах)
+ * Реализуйте:
+ * Конструктор с параметрами.
+ * Метод surfaceGravity(), вычисляющий гравитацию по формуле:
+ * G * mass / (radius * radius), где G = 6.67430e-11.
+ * В классе PlanetDemo выведите для каждой планеты её гравитацию.
+ */
+
+public enum Planet {
+    EARTH(3, 2), MARS(4, 5), JUPITER(6, 7);
+
+    private final double mass;
+    private final double radius;
+    private final static double G = 6.67430e-11;
+
+
+    Planet(double mass, double radius) {
+        this.mass = mass;
+        this.radius = radius;
+    }
+
+    public double surfaceGravity() {
+        return G * mass / (radius * radius);
+    }
+
+
 }
--- src/topic3Enums/task3Planet/PlanetDemo.java
+++ src/topic3Enums/task3Planet/PlanetDemo.java
@@ -1,2 +1,9 @@
-package topic3Enums.task3Planet;public class PlanetDemo {
+package topic3Enums.task3Planet;
+
+public class PlanetDemo {
+    public static void main(String[] args) {
+        for (Planet planet : Planet.values()) {
+            System.out.println(planet + ": " + planet.surfaceGravity());
+        }
+    }
 }
